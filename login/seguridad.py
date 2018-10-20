a8016c7dcb3e7fa8fc01f3cc49fe14d84fb11be8
Moisés González <11-10406@usb.ve>
2018-10-19 21:56:03 -0400

    Agregar función para validar direcciones de correo.
    Pruebas pasan.

--- login/seguridad.py
+++ login/seguridad.py
@@ -2,3 +2,13 @@ class Seguridad():
 
     def __init__(self):
         pass
+
+    # Método para validar una dirección de correo
+    def validar_correo(self,correo):
+        from django.core.validators import validate_email
+        from django.core.exceptions import ValidationError
+        try:
+            validate_email( correo )
+            return True
+        except ValidationError:
+            return False
