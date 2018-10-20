ce3cbf1673988b1e3fd28e3199ef5b6ce41a9fa3
Moisés González <11-10406@usb.ve>
2018-10-19 22:59:07 -0400

    Agregar comentarios

--- login/seguridad.py
+++ login/seguridad.py
@@ -3,7 +3,7 @@ class Seguridad():
     def __init__(self):
         self.diccionario = {}
 
-    # Método para validar una dirección de correo
+    # Usamos librerias de django para saber si el correo es valido
     def validar_correo(self,correo):
         from django.core.validators import validate_email
         from django.core.exceptions import ValidationError
@@ -13,6 +13,7 @@ class Seguridad():
         except ValidationError:
             return False
 
+    # Se asegura que coinciden y cumplen con los requisistos
     def validar_clave(self, clave, confirmacion):
         import re
         if clave != confirmacion:
@@ -21,6 +22,7 @@ class Seguridad():
         regex_clave  = re.compile('(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,16}$')
         return bool(regex_clave.match(clave))
 
+    # Agrega un par clave-valor a diccionario. El correo y la clave invertida
     def registrarUsuario(self, correo, clave, confirmacion):
         if not ( self.validar_correo(correo) and self.validar_clave(clave, confirmacion) ):
             return False
