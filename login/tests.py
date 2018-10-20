b57025f14e3205367692ad887a8d7dd3273b00e7
Moisés González <11-10406@usb.ve>
2018-10-19 21:43:27 -0400

    Agregar primera prueba del ciclo.
    Falla porque no hay código.

--- login/tests.py
+++ login/tests.py
@@ -4,5 +4,12 @@ from .seguridad import Seguridad
 # Create your tests here.
 class TestFree(TestCase):
 
-    def test_pass(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.s = Seguridad()
+
+    """Primer Ciclo TDD"""
+    # Frontera.
+    def test_verificar_correo_valido(self):
+        correo_valido = "moises@correo.com"
+        es_valido = self.s.validar_correo(correo_valido)
+        self.assertTrue(es_valido)
